f583e4e56a1e4c69cd0ba47eb00b5bf847ca488e
TimurKuanchileev <75033808+TimurKuanchileev@users.noreply.github.com>
2021-01-20 17:02:46 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
-import sys
 import os
 from pygame_widgets import Button
+from src.player import Player
 
 
 def load_image(name):
@@ -18,46 +18,142 @@ def menu_to_running():
     global running
     menu = False
     running = True
+    restart()
+
+
+def running_to_menu():
+    global menu
+    global running
+    global running_pause
+    menu = True
+    running = False
+    running_pause = False
+
+
+def unpause():
+    global running_pause
+    running_pause = False
+
+
+def restart():
+    global player
+    global all_sprites
+    global running_pause
+    player = Player()
+    all_sprites.add(player)
+    running_pause = False
 
 
 pygame.init()
+pygame.mixer.init()
+fps = 60
+clock = pygame.time.Clock()
 screen = pygame.display.set_mode((1280, 960))
+pause_b = pygame.Surface((750, 600))
+pause = pygame.Surface((750, 600))
 all_sprites = pygame.sprite.Group()
-b_start = Button(screen, 440, 200, 400, 70, text='Играть',
-                 fontSize=40, hoverColour=(78, 163, 39),
-                 inactiveColour=(50, 122, 17),
-                 pressedColour=(231, 247, 49), radius=20,
-                 textColour=(0, 0, 255),
-                 onClick=menu_to_running)
-b_hangar = Button(screen, 440, 350, 400, 70, text='Ангар',
-                  fontSize=40, hoverColour=(78, 163, 39),
-                  inactiveColour=(50, 122, 17),
-                  pressedColour=(231, 247, 49), radius=20,
-                  textColour=(0, 0, 255),
-                  onClick=lambda: print('Click'))
-b_top = Button(screen, 440, 500, 400, 70, text='Таблица рекордов',
-               fontSize=40, hoverColour=(78, 163, 39),
-               inactiveColour=(50, 122, 17),
-               pressedColour=(231, 247, 49), radius=20,
-               textColour=(0, 0, 255),
-               onClick=lambda: print('Click'))
-b_quit = Button(screen, 440, 650, 400, 70, text='Выйти из игры',
-                fontSize=40, hoverColour=(78, 163, 39),
-                inactiveColour=(50, 122, 17),
-                pressedColour=(231, 247, 49), radius=20,
-                textColour=(0, 0, 255),
-                onClick=lambda: pygame.quit())
+pygame.mouse.set_visible(False)
+menu_b_start = Button(screen, 440, 200, 400, 70, text='Играть',
+                      fontSize=40, hoverColour=(78, 163, 39),
+                      inactiveColour=(50, 122, 17),
+                      pressedColour=(231, 247, 49), radius=20,
+                      textColour=(0, 0, 255),
+                      onClick=menu_to_running)
+menu_b_hangar = Button(screen, 440, 350, 400, 70, text='Ангар',
+                       fontSize=40, hoverColour=(78, 163, 39),
+                       inactiveColour=(50, 122, 17),
+                       pressedColour=(231, 247, 49), radius=20,
+                       textColour=(0, 0, 255),
+                       onClick=lambda: print('Click'))
+menu_b_top = Button(screen, 440, 500, 400, 70, text='Таблица рекордов',
+                    fontSize=40, hoverColour=(78, 163, 39),
+                    inactiveColour=(50, 122, 17),
+                    pressedColour=(231, 247, 49), radius=20,
+                    textColour=(0, 0, 255),
+                    onClick=lambda: print('Click'))
+menu_b_quit = Button(screen, 440, 650, 400, 70, text='Выйти из игры',
+                     fontSize=40, hoverColour=(78, 163, 39),
+                     inactiveColour=(50, 122, 17),
+                     pressedColour=(231, 247, 49), radius=20,
+                     textColour=(0, 0, 255),
+                     onClick=lambda: pygame.quit())
+pause_b_coninue = Button(pause_b, 50, 360, 300, 60, text='Продолжить',
+                         fontSize=40, hoverColour=(78, 163, 39),
+                         inactiveColour=(50, 122, 17),
+                         pressedColour=(231, 247, 49), radius=20,
+                         textColour=(0, 0, 255),
+                         onClick=lambda: unpause())
+pause_b_restart = Button(pause_b, 50, 500, 300, 60, text='Заново',
+                         fontSize=40, hoverColour=(78, 163, 39),
+                         inactiveColour=(50, 122, 17),
+                         pressedColour=(231, 247, 49), radius=20,
+                         textColour=(0, 0, 255),
+                         onClick=lambda: restart())
+pause_b_menu = Button(pause_b, 400, 360, 300, 60, text='В главное меню',
+                      fontSize=40, hoverColour=(78, 163, 39),
+                      inactiveColour=(50, 122, 17),
+                      pressedColour=(231, 247, 49), radius=20,
+                      textColour=(0, 0, 255),
+                      onClick=lambda: running_to_menu())
+pause_b_quit = Button(pause_b, 400, 500, 300, 60, text='Выйти из игры',
+                      fontSize=40, hoverColour=(78, 163, 39),
+                      inactiveColour=(50, 122, 17),
+                      pressedColour=(231, 247, 49), radius=20,
+                      textColour=(0, 0, 255),
+                      onClick=lambda: pygame.quit())
 running = False
+running_pause = False
 menu = True
+cursor = load_image('arrow.png').convert_alpha()
 while True:
     while running:
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        for event in events:
             if event.type == pygame.QUIT:
+                pygame.quit()
                 running = False
                 quit()
-        bg_running = pygame.transform.scale(
-            load_image('backgrounds\\space.png'), (1280, 960))
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running_pause = True
+                if not running_pause:
+                    if event.key == pygame.K_LEFT:
+                        player.direction = 5
+                    if event.key == pygame.K_RIGHT:
+                        player.direction = -5
+            if event.type == pygame.KEYUP:
+                if not running_pause:
+                    if event.key == pygame.K_LEFT:
+                        player.direction = 0
+                    if event.key == pygame.K_RIGHT:
+                        player.direction = 0
+        bg_running = pygame.transform.scale(load_image('backgrounds\\space.jpg'), (1280, 960))
         screen.blit(bg_running, (0, 0))
+        screen.blit(player.image, (player.rect.x + 500, player.rect.y + 500))
+        if running_pause:
+            pause.fill((0, 80, 199))
+            pause.set_alpha(75)
+            screen.blit(pause, (240, 100))
+            pause_b.set_colorkey('BLACK')
+            screen.blit(pause_b, (240, 100))
+            pause_b_coninue.listen(events)
+            pause_b_coninue.draw()
+            pause_b_restart.listen(events)
+            pause_b_restart.draw()
+            pause_b_menu.listen(events)
+            pause_b_menu.draw()
+            pause_b_quit.listen(events)
+            pause_b_quit.draw()
+            x, y = pygame.mouse.get_pos()
+            pygame.draw.rect(pause_b, (0, 0, 255), (0, 0, 750, 600), 15)
+            pygame.draw.rect(pause_b, (0, 0, 255), (403, 503, 295, 55), 7)
+            pygame.draw.rect(pause_b, (0, 0, 255), (403, 363, 295, 55), 7)
+            pygame.draw.rect(pause_b, (0, 0, 255), (53, 363, 295, 55), 7)
+            pygame.draw.rect(pause_b, (0, 0, 255), (53, 503, 295, 55), 7)
+            screen.blit(cursor, (x + 240, y + 100))
+        else:
+            player.update()
+        clock.tick(fps)
         pygame.display.update()
     while menu:
         events = pygame.event.get()
@@ -66,19 +162,20 @@ while True:
                 pygame.quit()
                 menu = False
                 quit()
-        bg_menu = pygame.transform.scale(
-            load_image('backgrounds\\menu.jpg'), (1280, 960))
+        x, y = pygame.mouse.get_pos()
+        bg_menu = pygame.transform.scale(load_image('backgrounds\\menu.jpg'), (1280, 960))
         screen.blit(bg_menu, (0, 0))
-        b_start.listen(events)
-        b_start.draw()
-        b_hangar.listen(events)
-        b_hangar.draw()
-        b_top.listen(events)
-        b_top.draw()
-        b_quit.listen(events)
-        b_quit.draw()
+        menu_b_start.listen(events)
+        menu_b_start.draw()
+        menu_b_hangar.listen(events)
+        menu_b_hangar.draw()
+        menu_b_top.listen(events)
+        menu_b_top.draw()
+        menu_b_quit.listen(events)
+        menu_b_quit.draw()
         pygame.draw.rect(screen, (0, 0, 255), (440, 200, 400, 70), 11)
         pygame.draw.rect(screen, (0, 0, 255), (440, 350, 400, 70), 11)
         pygame.draw.rect(screen, (0, 0, 255), (440, 500, 400, 70), 11)
         pygame.draw.rect(screen, (0, 0, 255), (440, 650, 400, 70), 11)
+        screen.blit(cursor, (x, y))
         pygame.display.update()
---
+++ src/player.py
@@ -0,0 +1,70 @@
+import os
+import pygame
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.ship = pygame.image.load(os.path.join('images\\Ships\\begin\\1.png')).convert_alpha()
+        self.image = self.ship
+        self.rect = self.ship.get_rect()
+        self.rot = 0
+        self. speedy = 0
+        self.speedx = 0
+        self.last_update = pygame.time.get_ticks()
+        self.direction = 0
+
+    def update(self):
+        speed =self.get_speed()
+        if self.rect.x > 0 and speed[0] > 0:
+            if speed[0] % 1 != 0:
+                speed[0] += 1
+        if self.rot in [0, 180]:
+            speed[0] = 0
+        self.rect.x += speed[0]
+        if self.rect.y > 0 and speed[1] > 0:
+            if speed[1] % 1 != 0:
+                speed[1] += 1
+        if self.rot in [90, 270]:
+            speed[1] = 0
+        self.rect.y += speed[1]
+        self.rotate()
+
+    def rotate(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > 50:
+            self.last_update = now
+            self.rot = (self.rot + self.direction) % 360
+            self.image = pygame.transform.rotate(self.ship, self.rot)
+            self.center = self.rect.center
+            self.rect = self.image.get_rect()
+            self.rect.center = self.center
+
+    def get_speed(self):
+        if self.rot <= 180:
+             speedy = (self.rot - 90) / 20
+        else:
+            speedy = -(self.rot - 270) / 20
+        if 90 < self.rot < 270:
+            speedx = (self.rot - 180) / 20
+        elif self.rot > 90:
+            speedx = -(self.rot - 360) / 20
+        else:
+            speedx = -(self.rot) / 20
+        if -1 < speedy < 1:
+            self.speedy += speedy
+            if -1 < self.speedy < 1:
+                speedy = 0
+            else:
+                speedy = self.speedy
+        if -1 < speedx < 1:
+            self.speedx += speedx
+            if -1 < self.speedx < 1:
+                speedx = 0
+            else:
+                speedx = self.speedx
+        if 1 < self.speedy or -1 > self.speedy:
+            self.speedy = 0
+        if 1 < self.speedx or -1 > self.speedx:
+            self.speedx = 0
+        return [speedx, speedy]
